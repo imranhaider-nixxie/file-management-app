7c237b2dd0d14b49952efbb9c9224745bc01fab8
Imran Haider Muzaffar <152253810+imranhaider-nixxie@users.noreply.github.com>
2024-10-29 18:10:48 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectDB();
 // Use CORS
 
 const corsOptions = {
-  origin: 'http://localhost:3000', // Allow only this origin
+  origin: 'http://142.93.160.50:3000', // Allow only this origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
   credentials: true, // Include credentials (cookies, authorization headers, etc.)
 };
